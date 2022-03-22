5d26a385e1fa73e144967d4168052095dc0d9c18
markdenardo <mark.denardo@gmail.com>
2022-03-21 20:27:02 -0400

    scope fix

--- storyGenerator.js
+++ storyGenerator.js
@@ -4,37 +4,34 @@
 //2. run node
 //3. copy code paste in node
 //4. there are two commands: start() and add(array, string)
-//5. start() -- runs the code and generates a new story
+//5. makeStory() -- runs the code and generates a new story
 //6. add(array,string) -- adds new strings() to assigned array (character, setting, or plot) example: add(character, 'Ada Lovelace')
 
-function start() {
-  const response = false;
-  const character = ["dog", "cat", "person"];
-  const setting = ["mars", "baseball field", "swimming pool"];
-  const plot = ["dinner", "walk", "fighting"];
+const response = false;
+const character = ["dog", "cat", "person"];
+const setting = ["mars", "baseball field", "swimming pool"];
+const plot = ["dinner", "walk", "fighting"];
 
-  const rand = (array) => {
-    return array[Math.floor(Math.random() * array.length)];
-  };
+const rand = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
+};
 
-  const add = (array, string) => {
-    array.push(string);
-  };
+const add = (array, string) => {
+  array.push(string);
+};
 
-  const story = (character, setting, plot) => {
-    const state =
-      ((story.character = character),
-      (story.setting = setting),
-      (story.plot = plot));
-  };
+const story = (character, setting, plot) => {
+  const state =
+    ((story.character = character),
+    (story.setting = setting),
+    (story.plot = plot));
+};
 
-  const makeStory = (story) => {
-    console.log(
-      `A ${rand(character)} went to ${rand(setting)} for ${rand(plot)}`
-    );
-  };
+const makeStory = (story) => {
+  console.log(
+    `A ${rand(character)} went to ${rand(setting)} for ${rand(plot)}`
+  );
+};
 
-  story(character, setting, plot);
-  makeStory(story);
-}
-start();
+story(character, setting, plot);
+makeStory(story);
